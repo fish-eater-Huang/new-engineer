12f31ba0de5dcd46d16869e7bc35461274f78499
spoonguan <285846897@qq.com>
2023-06-19 03:45:48 +0800

    feature: 控制器陀螺仪数据发送+陀螺仪偏移校准

--- controller/app/can_monitor.cpp
+++ controller/app/can_monitor.cpp
@@ -18,6 +18,7 @@
 #include "base/cap_comm/cap_comm.h"
 
 extern DJIMotorDriver dji_motor_driver;
+extern BoardComm board_comm;
 
 // CAN filter初始化
 void canFilterInit(void) {
@@ -46,11 +47,7 @@ void canFilterInit(void) {
 // CAN通信发送管理
 void canTxMonitor(void) {
   // note: 每个通道一次只能发送3个包
-  dji_motor_driver.canTxMsg(1, djimotor::ID_1_4);   // can1, id: 0x200
-  dji_motor_driver.canTxMsg(1, djimotor::ID_5_8);   // can1, id: 0x1ff
-
-  dji_motor_driver.canTxMsg(2, djimotor::ID_1_4);  // can2, id: 0x200
-  dji_motor_driver.canTxMsg(2, djimotor::ID_5_8);  // can2, id: 0x1ff
+  board_comm.canTxMsg();
 
   osDelay(1);
 }
