931500ef84a9235d664236a548a5a98e82d75604
spoonguan <285846897@qq.com>
2023-06-29 13:02:02 +0800

    refactor: 控制器相关程序移至单独的文件以便后续逻辑、通信等开发和优化

--- engineer/app/arm_controller.h
+++ engineer/app/arm_controller.h
@@ -11,4 +11,48 @@
 #ifndef ARM_CONTROLLER_H
 #define ARM_CONTROLLER_H
 
+#include "app/imu_monitor.h"
+#include "base/robotics/robotics.h"
+
+// 机械臂控制器(3*imu)
+class ArmController {
+ public:
+  // 构造函数
+  ArmController(IMU imu[3]);
+
+  // 设置偏置值
+  void setOffset(float dx, float dy, float dz);
+
+  // 控制器处理函数
+  void handle(void);
+
+ public:
+  // 目标状态
+  struct Ref_t {
+    Matrixf<4, 4> T;
+
+    float x, y, z;           // m
+    float yaw, pitch, roll;  // rad
+  } ref_;
+
+  // 状态偏置
+  struct Raw_t {
+    float x, y, z;           // m
+    float yaw, pitch, roll;  // rad
+  } raw_;
+
+  // 状态偏置
+  struct Offset_t {
+    float x, y, z;           // m
+    float yaw, pitch, roll;  // rad
+  } offset_;
+
+ private:
+  IMU* imu_[3];
+
+  struct Param {
+    float l[2] = {0.3f, 0.27f};
+  } param_;
+};
+
 #endif  // ARM_CONTROLLER_H
