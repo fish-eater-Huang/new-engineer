918c791d0a08b9cfd6cabd00a75ab7921a496fe7
spoonguan <285846897@qq.com>
2023-07-02 15:10:48 +0800

    feature: J0云台版本云台程序初步完成（todo: 机械臂版本云台程序）

--- engineer/app/gimbal.cpp
+++ engineer/app/gimbal.cpp
@@ -12,146 +12,98 @@
 #include "base/common/math.h"
 #include "lib/arm_math/arm_math.h"
 
-// Encoder value of zero point. 零点编码器值
-const float yaw_zero_ecd = math::ecd2deg(995, 8192);
-const float pitch_zero_ecd = math::ecd2deg(6400, 8192);
+J0Gimbal::J0Gimbal(Motor* jm0, Motor* gm_yaw, Motor* gm_pitch, IMU* j0_imu)
+    : jm0_(jm0), gm_yaw_(gm_yaw), gm_pitch_(gm_pitch), j0_imu_(j0_imu) {}
 
-// Speed limit for initialization(dps). 云台初始化限速(dps)
-const float gimbal_init_speed_max = 180;
-const float gimbal_speed_max = 1800;
-// Gimbal init finish threshold. 云台初始化完成角度阈值
-const float gimbal_init_angle_thres = 5;
-
-// Gimbal angle limit 云台角度限位
-const float yaw_min = -60;
-const float yaw_max = 60;
-const float pitch_min = -90;
-const float pitch_max = 90;
-
-Gimbal::Gimbal(Motor* gm_yaw, Motor* gm_pitch, IMU* imu)
-    : gm_yaw_(gm_yaw), gm_pitch_(gm_pitch), imu_(imu) {
-  init_status_.yaw_finish = false;
-  init_status_.pitch_finish = false;
-  limit_.yaw_min = yaw_min;
-  limit_.yaw_max = yaw_max;
-  limit_.pitch_min = pitch_min;
-  limit_.pitch_max = pitch_max;
-  setMode(ENCODER_MODE);
+// J0初始化
+void J0Gimbal::initJ0(void) {
+  init_.j0_finish = false;
+  // 设置J0反馈角度为编码器单圈角度
+  jm0_->motor_data_.angle = math::degNormalize180(
+      (jm0_->motor_data_.ecd_angle - param_.j0_zero) / jm0_->ratio_);
+  jm0_->resetFeedbackAngle(jm0_->motor_data_.angle);
+  // 设置反馈数据源为陀螺仪
+  jm0_->setFdbSrc(&j0_imu_->yaw(), &j0_imu_->wzWorld());
 }
 
-// Initialize gimbal, reset init flag
-// 云台初始化，重置回正标记
-void Gimbal::init(void) {
-  init_status_.yaw_finish = false;
-  init_status_.pitch_finish = false;
-
-  // 初始化过程中设置目标角度为0，反馈角度为编码器单圈角度
-  ref_.yaw = 0;
-  ref_.pitch = 0;
-  gm_yaw_->motor_data_.angle = math::degNormalize180(
-      (gm_yaw_->motor_data_.ecd_angle - yaw_zero_ecd) / gm_yaw_->ratio_);
-  gm_yaw_->resetFeedbackAngle(gm_yaw_->motor_data_.angle);
-  gm_pitch_->motor_data_.angle = math::degNormalize180(
-      (gm_pitch_->motor_data_.ecd_angle - pitch_zero_ecd) / gm_pitch_->ratio_);
-  gm_pitch_->resetFeedbackAngle(gm_pitch_->motor_data_.angle);
+// yaw, pitch初始化
+void J0Gimbal::initGM(void) {
+  init_.yaw_finish = false;
+  init_.pitch_finish = false;
+  // 设置反馈数据源
+  gm_yaw_->setFdbSrc(&gm_yaw_->kfAngle(), &gm_yaw_->kfSpeed());
+  gm_pitch_->setFdbSrc(&gm_pitch_->kfAngle(), &gm_pitch_->kfSpeed());
 }
 
-// Set gimbal angle and speed
-// 设置云台角度&速度(deg, dps)
-void Gimbal::setAngleSpeed(const float& yaw, const float& pitch,
-                           const float& yaw_speed, const float& pitch_speed) {
-  // 设置目标状态
-  ref_.yaw = yaw;
-  ref_.pitch = math::limit(pitch,
-                           gm_pitch_->control_data_.fdb_angle -
-                               gm_pitch_->motor_data_.angle + limit_.pitch_min,
-                           gm_pitch_->control_data_.fdb_angle -
-                               gm_pitch_->motor_data_.angle + limit_.pitch_max);
-  ref_.yaw_speed = yaw_speed;
-  ref_.pitch_speed = pitch_speed;
+// 全部初始化
+void J0Gimbal::initAll(void) {
+  initJ0();
+  initGM();
 }
 
-// Set gimbal angle
 // 设置云台角度(deg)
-void Gimbal::setAngle(const float& yaw, const float& pitch) {
-  setAngleSpeed(yaw, pitch, 0, 0);
+void J0Gimbal::setAngle(const float& j0, const float& yaw, const float& pitch) {
+  ref_.j0 = j0;
+  ref_.yaw = math::limit(yaw, param_.yaw_min, param_.yaw_max);
+  ref_.pitch = math::limit(pitch, param_.pitch_min, param_.pitch_max);
 }
 
-// Add gimbal angle
 // 设置云台角度增量(deg)
-void Gimbal::addAngle(const float& d_yaw, const float& d_pitch) {
+void J0Gimbal::addAngle(const float& j0, const float& yaw, const float& pitch) {
   // 设置目标状态
-  ref_.yaw += d_yaw;
-  ref_.pitch = math::limit(ref_.pitch + d_pitch,
-                           gm_pitch_->control_data_.fdb_angle -
-                               gm_pitch_->motor_data_.angle + limit_.pitch_min,
-                           gm_pitch_->control_data_.fdb_angle -
-                               gm_pitch_->motor_data_.angle + limit_.pitch_max);
+  setAngle(ref_.j0 + j0, ref_.yaw + yaw, ref_.pitch + pitch);
 }
 
-// Set gimbal mode(imu)
-// 设置云台模式(imu反馈或编码器反馈)
-void Gimbal::setMode(GimbalFdbMode_e mode) {
-  if (mode_ != mode && mode == IMU_MODE) {
-    gm_yaw_->setFdbSrc(&imu_->yaw(), &imu_->wzWorld());
-    gm_pitch_->setFdbSrc(&imu_->pitch(), &imu_->wySensor());
-  } else if (mode_ != mode && mode == ENCODER_MODE) {
-    gm_yaw_->setFdbSrc(&gm_yaw_->kfAngle(), &gm_yaw_->kfSpeed());
-    gm_pitch_->setFdbSrc(&gm_pitch_->kfAngle(), &gm_pitch_->kfSpeed());
-  }
-  mode_ = mode;
-}
-
-// Send target status to motor and update feedback
 // 设置电机目标状态，更新反馈数据
-void Gimbal::handle(void) {
-  // 云台初始化慢速回正
-  if (!init_status_.yaw_finish) {  // yaw初始化未完成
-    // 判断复位是否完成
-    init_status_.yaw_finish =
-        gm_yaw_->connect_.check() &&
-        (fabs(gm_yaw_->motor_data_.angle) < gimbal_init_angle_thres);
-    if (init_status_.yaw_finish) {
-      // 检测到初始化完成，速度限制恢复正常
-      gm_yaw_->ppid_.out_max_ = gimbal_speed_max;
-    } else {
-      // 设置初始化速度限制
-      gm_yaw_->ppid_.out_max_ = gimbal_init_speed_max;
+void J0Gimbal::handle(void) {
+  // J0初始化，更新反馈角度并回正
+  if (!init_.j0_finish) {
+    ref_.j0 = 0;
+    init_.j0_finish = jm0_->connect_.check() &&
+                      (fabs(gm_yaw_->motor_data_.angle) < init_.j0_thres);
+  }
+  // yaw初始化
+  if (!init_.yaw_finish) {
+    ref_.yaw -= 9e-2f;
+    if (gm_yaw_->connect_.check() &&
+        gm_yaw_->targetAngle() - gm_yaw_->realAngle() < -10) {
+      // 初始化完成
+      gm_yaw_->resetFeedbackAngle(init_.yaw_angle);
+      ref_.yaw = 0;
+      init_.yaw_finish = true;
     }
   }
-  if (!init_status_.pitch_finish) {  // pitch初始化未完成
-    // 判断复位是否完成
-    init_status_.pitch_finish =
-        gm_pitch_->connect_.check() &&
-        (fabs(gm_pitch_->motor_data_.angle) < gimbal_init_angle_thres);
-    if (init_status_.pitch_finish) {
-      // 检测到初始化完成，速度限制恢复正常
-      gm_pitch_->ppid_.out_max_ = gimbal_speed_max;
-    } else {
-      // 设置初始化速度限制
-      gm_pitch_->ppid_.out_max_ = gimbal_init_speed_max;
+  // pitch初始化
+  if (!init_.pitch_finish) {
+    ref_.pitch -= 9e-2f;
+    if (gm_pitch_->connect_.check() &&
+        gm_pitch_->targetAngle() - gm_pitch_->realAngle() < -10) {
+      // 初始化完成
+      gm_pitch_->resetFeedbackAngle(init_.pitch_angle);
+      ref_.pitch = 0;
+      init_.pitch_finish = true;
     }
   }
 
+  // J0电机离线处理
+  if (!jm0_->connect_.check()) {
+    initJ0();
+  }
   // 云台电机离线处理
   if (!gm_yaw_->connect_.check() && !gm_pitch_->connect_.check()) {
-    init();
+    initGM();
   }
 
-  // 读取电机控制反馈
-  fdb_.yaw = gm_yaw_->control_data_.fdb_angle;
-  fdb_.pitch = gm_pitch_->control_data_.fdb_angle;
-  fdb_.yaw_speed = gm_yaw_->control_data_.fdb_speed;
-  fdb_.pitch_speed = gm_pitch_->control_data_.fdb_speed;
+  // 读取电机反馈
+  fdb_.j0 = jm0_->realAngle();
+  fdb_.j0_speed = jm0_->realSpeed();
+  fdb_.yaw = gm_yaw_->realAngle();
+  fdb_.yaw_speed = gm_yaw_->realSpeed();
+  fdb_.pitch = gm_pitch_->realAngle();
+  fdb_.pitch_speed = gm_pitch_->realSpeed();
 
-  // 设置电机控制目标角度 & 角速度前馈 & 力矩前馈(补偿)
-  gm_yaw_->setAngleSpeed(ref_.yaw, ref_.yaw_speed);
-  gm_pitch_->setAngleSpeed(ref_.pitch, ref_.pitch_speed,
-                           pitchCompensate(fdb_.pitch));
-}
-
-// pitch轴力矩补偿(作为力矩前馈输入)
-float Gimbal::pitchCompensate(const float& pitch) {
-  pitch_compensate_ = 0;
-  return pitch_compensate_;
+  // 设置电机控制目标角度
+  jm0_->setAngle(ref_.j0);
+  gm_yaw_->setAngle(ref_.yaw);
+  gm_pitch_->setAngle(ref_.pitch);
 }
--- engineer/app/gimbal.h
+++ engineer/app/gimbal.h
@@ -11,99 +11,80 @@
 #ifndef GIMBAL_H
 #define GIMBAL_H
 
+#include "app/arm.h"
 #include "base/imu/imu.h"
 #include "base/motor/motor.h"
 
-// 云台反馈模式(imu/编码器)
-typedef enum GimbalFdbMode {
-  IMU_MODE,
-  ENCODER_MODE,
-} GimbalFdbMode_e;
-
-// 云台运动状态
-typedef struct GimbalStatus {
-  float yaw;
-  float pitch;
-  float yaw_speed;
-  float pitch_speed;
-} GimbalStatus_t;
-
-// 云台类
-class Gimbal {
+// J0云台类
+class J0Gimbal {
  public:
-  Gimbal(Motor* gm_yaw, Motor* gm_pitch, IMU* imu);
+  J0Gimbal(Motor* jm0, Motor* gm_yaw, Motor* gm_pitch, IMU* j0_imu);
 
-  // Initialize gimbal, reset init flag
-  // 云台初始化，重置回正标记
-  void init(void);
+  // J0初始化
+  void initJ0(void);
+  // yaw, pitch初始化
+  void initGM(void);
+  // 全部初始化
+  void initAll(void);
 
-  // 云台初始化判断
-  bool initFinish(void) {
-    return init_status_.yaw_finish && init_status_.pitch_finish;
-  }
-
-  // Set gimbal angle and speed
-  // 设置云台角度&速度(deg, dps)
-  void setAngleSpeed(const float& yaw, const float& pitch,
-                     const float& yaw_speed = 0, const float& pitch_speed = 0);
-
-  // Set gimbal angle
   // 设置云台角度(deg)
-  void setAngle(const float& yaw, const float& pitch);
-
-  // Add gimbal angle
+  void setAngle(const float& j0, const float& yaw, const float& pitch);
   // 设置云台角度增量(deg)
-  void addAngle(const float& d_yaw, const float& d_pitch);
+  void addAngle(const float& j0, const float& yaw, const float& pitch);
 
-  // Set gimbal mode(imu)
-  // 设置云台模式(imu反馈或编码器反馈)
-  void setMode(GimbalFdbMode_e mode);
-
-  // Send target status to motor and update feedback
   // 设置电机目标状态，更新反馈数据
   void handle(void);
 
- private:
-  // pitch轴力矩补偿(作为力矩前馈输入)
-  float pitchCompensate(const float& pitch);
-
  public:
-  Motor *gm_yaw_, *gm_pitch_;  // 电机指针
-  IMU* imu_;                   // imu指针
+  // 云台/J0参数
+  struct Param_t {
+    // 编码器零点
+    const float j0_zero = 0;
 
-  // 目标状态数据
-  GimbalStatus_t ref_;
-  // 反馈状态数据
-  GimbalStatus_t fdb_;
-  // 控制模式
-  GimbalFdbMode_e mode_;
-  // pitch轴补偿
-  float pitch_compensate_;
-  // 角度限位(deg)
-  struct GimbalAngleLimit_t {
-    float yaw_min;
-    float yaw_max;
-    float pitch_min;
-    float pitch_max;
-  } limit_;
+    // 云台角度限位(°)
+    const float yaw_min = -180;
+    const float yaw_max = 180;
+    const float pitch_min = -90;
+    const float pitch_max = 90;
 
-  // 上电按编码器初始化相关参数
-  struct InitStatus_t {
+    // 初始化
+    const float j0_speed_limit = 360;
+    const float gm_speed_limit = 720;
+  } param_;
+
+  // 初始化相关
+  struct Init_t {
+    bool j0_finish;
     bool yaw_finish;
     bool pitch_finish;
-  } init_status_;
-};
 
-// 大yaw平台
-class Platform {
- public:
-  Platform(Motor* motor, IMU* imu);
+    const float j0_thres = 5;
+    const float yaw_angle = -90;
+    const float pitch_angle = -90;
+  } init_;
+
+  // 目标状态数据
+  struct Ref_t {
+    float j0;
+    float yaw;
+    float pitch;
+  } ref_;
+
+  // 反馈状态数据
+  struct Fdb_t {
+    float j0;
+    float yaw;
+    float pitch;
+    float j0_speed;
+    float yaw_speed;
+    float pitch_speed;
+  } fdb_;
 
  private:
-  Motor* motor_;  // 电机指针
-  IMU* imu_;      // imu指针
+  // 电机指针
+  Motor *jm0_, *gm_yaw_, *gm_pitch_;
+  // imu指针
+  IMU* j0_imu_;
 };
-
-extern const float yaw_zero_ecd, pitch_zero_ecd;
 
 #endif  // GIMBAL_H
